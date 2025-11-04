969c1a00921606d52868da0b63ac856cfb9b4903
Ricardo Monsalve <videsapp@Ricardos-MacBook-Pro.local>
2025-11-04 12:39:51 -0300

    restore(scrapers): volver a versión previa al scrub para recuperar precios

--- scrapers/salcobrand.js
+++ scrapers/salcobrand.js
@@ -1,2 +1,6 @@
+// scrapers/salcobrand.js
 export const sourceId = 'salcobrand';
-export async function fetchSalcobrand(_q,_ctx){ return []; }
+
+export async function fetchSalcobrand(_page, _key) {
+  return [];
+}
